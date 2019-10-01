5de133a3d2a0f1676bc5cba4714287eba614ed4b
andre de oliveira <andre_silveira-juni1@estudante.sc.senai.br>
2019-09-30 21:18:07 -0300

    Formulario de cadastro finalizado
    Close #3

--- index.php
+++ index.php
@@ -14,6 +14,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de login</title>
+    <style>
+    #alerta, #caixaSenha, #caixaRegistro{
+        display:none;
+    }
+    </style>
 </head>
 
 <body class="bg-dark">
@@ -61,7 +66,7 @@
                 </div>
                 <div class="form-group">
                     <p>
-                        Novo usuario? <a href="#" id="btnRegistrar">
+                        Novo usuario? <a href="#" id="btnRegistrarNovo">
                             Registre-se aqui!
                         </a>
                     </p>
@@ -73,7 +78,7 @@
         <br>
         <!-- Formulario de recuperaçao de senha -->
         <section class="row mt-5">
-            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
                 <h2 class="text-center mt-2">
                     Gerar Nova Senha
                 </h2>
@@ -110,23 +115,48 @@
         <section class="row mt-5">
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
                 <h2 class="text-center mt-2">Registra-se aqui</h2>
-                <form action="#" method="post" class="pt-2" id="formRegistro">
+                <form action="#" method="post" class="p-2" id="formRegistro">
 
                     <div class="form-group">
                         <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo">
                     </div>
 
-                    <div class="form=group">
+                    <div class="form-group">
                         <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de usuário">
                     </div>
 
 
-                    <div class="form=group">
+                    <div class="form-group">
                         <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail">
                     </div>
 
-                    <div class="form=group">
+                    <div class="form-group">
                         <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar senha">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
+                            <label for="concordar" class="custom-control-label">
+                                Eu concordo com <a href="#"> os termos e condiçoes
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="::Registrar::" name="btnRegistrar" id="btnregistrar" class="btn btn-primary btn-block">
+                    </div>
+
+                    <div class="form-group">
+                        <p class="text-center">
+                            Já registrado? <a href="#" id="btnJaRegistrado2">
+                                Entrar por aqui.
+                            </a>
+                        </p>
                     </div>
 
 
@@ -146,6 +176,10 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+    <script>
+        //codigo jQuery para mostrar e ocultar os formularios
+    </script>
 </body>
 
 </html>
